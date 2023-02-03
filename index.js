57e52ffe235fb2e14fcff2b574246420b1bcbb7a
Carl_Javier <ccjavier87@gmail.com>
2023-02-03 11:47:08 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+    return 264 * Math.abs(street - 42);
+}
+
+function distanceTravelledInFeet(start, fin) {
+    return 264 * Math.abs(fin - start);
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = 264 * Math.abs(destination - start);
+    let charge;
+    if (feet <= 400) {
+        return 0;
+    } else if (feet > 400 && feet <= 2000) {
+        return 0.02 * Math.floor(Math.abs(400 - feet));
+    } else if (feet > 2000 && feet <= 2500) {
+        return 25;
+    } else if (feet > 2500) {
+        return 'cannot travel that far';
+    }
+
+
+}
